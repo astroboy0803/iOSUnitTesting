9b70b6cf43f33a5f407e5c78ab682664477fe492
黃宇新 <astroboy03@cathaylife.com.tw>
2022-05-01 15:40:38 +0800

    Test Life Cycle

---
+++ TestLifeCycle/TestLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestLifeCycle
+//
+//  Created by BruceHuang on 2022/5/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
