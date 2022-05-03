2e2e4a5e270f74871c5a013f9331236baeb66c38
黃宇新 <astroboy03@cathaylife.com.tw>
2022-05-03 10:23:26 +0800

    coverage

---
+++ CodeCoverage/CodeCoverage/CoveredClass.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+class CoveredClass {
+    static func max(_ x: Int, _ y: Int) -> Int {
+        if x < y {
+            return y
+        } else {
+            return x
+        }
+    }
+    
+    static func commaSeparated(from: Int, to: Int) -> String {
+        var result = ""
+        for i in from..<to {
+            result += "\(i),"
+        }
+        result += "\(to)"
+        return result
+    }
+    
+    private(set) var area: Int
+    
+    var width: Int {
+        didSet {
+            area = width * width
+            let color: UIColor = redOrGreen(for: width)
+            drawSquare(width: width, color: color)
+        }
+    }
+    
+    init() {
+        width = 0
+        area = 0
+    }
+    
+    private func redOrGreen(for width: Int) -> UIColor {
+        width % 2 == 0 ? .red : .green
+    }
+    
+    private func drawSquare(width: Int, color: UIColor) {
+        // ...
+    }
+    
+}
---
+++ CodeCoverage/CodeCoverageTests/CoveredClassTests.swift
@@ -0,0 +1,54 @@
+//
+//  CoveredClassTests.swift
+//  CodeCoverageTests
+//
+//  Created by BruceHuang on 2022/5/1.
+//
+
+import XCTest
+@testable import CodeCoverage
+
+class CoveredClassTests: XCTestCase {
+    // characterization test
+    // 1. call from the test and get result
+    // 2. assertion
+    // 3. run the test
+    // 4. adjustment
+    // 5. test pass
+    
+//    func test_max_with1And2_shouldReturnSomething() {
+//        let result = CoveredClass.max(1, 2)
+//        XCTAssertEqual(result, -123)
+//    }
+    
+    // good test name
+    // 1. what the test is exercising -> function name
+    // 2. the condition of the test -> input information
+    // 3. the expected result
+    
+    func test_max_with1And2_shouldReturn2() {
+        let result = CoveredClass.max(1, 2)
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_max_with3And2_shouldReturn3() {
+        let result = CoveredClass.max(3, 2)
+            XCTAssertEqual(result, 3)
+    }
+    
+    func test_commaSeparated_from2to4_shouldReturn234SeparatedByCommas() {
+        let result = CoveredClass.commaSeparated(from: 2, to: 4)
+        XCTAssertEqual(result, "2,3,4")
+    }
+    
+    func test_commaSeparated_from2to4_shouldReturn2WithNoComma() {
+        let result = CoveredClass.commaSeparated(from: 2, to: 2)
+        XCTAssertEqual(result, "2")
+    }
+    
+    func test_area_withWidth7_shouldBe49() {
+        let sut = CoveredClass()
+        sut.width = 7
+        XCTAssertEqual(sut.area, 49)
+    }
+}
