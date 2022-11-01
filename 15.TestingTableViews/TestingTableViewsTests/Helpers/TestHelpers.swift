f8b17c0d67ae9fe19c15c37ab36ed162d432d722
黃宇新 <astroboy0803@gmail.com>
2022-11-01 17:05:46 +0800

    ch 15

---
+++ 15.TestingTableViews/TestingTableViewsTests/Helpers/TestHelpers.swift
@@ -0,0 +1,88 @@
+import Foundation
+import UIKit
+
+func executeRunLoop() {
+    RunLoop.current.run(until: Date())
+}
+
+func putInWindow(_ vc: UIViewController) {
+    let window: UIWindow = .init()
+    window.rootViewController = vc
+    window.isHidden = false
+}
+
+func putInViewHeirarychy(_ vc: UIViewController) {
+    let window: UIWindow = .init()
+    window.addSubview(vc.view)
+}
+
+extension UITextContentType: CustomStringConvertible {
+    public var description: String {
+        rawValue
+    }
+}
+
+extension UITextAutocorrectionType: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .default:
+            return "default"
+        case .no:
+            return "no"
+        case .yes:
+            return "yes"
+        }
+    }
+}
+
+extension UIReturnKeyType: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .default:
+            return "default"
+        case .go:
+            return "go"
+        case .google:
+            return "google"
+        case .join:
+            return "join"
+        case .next:
+            return "next"
+        case .route:
+            return "route"
+        case .search:
+            return "search"
+        case .send:
+            return "send"
+        case .yahoo:
+            return "yahoo"
+        case .done:
+            return "done"
+        case .emergencyCall:
+            return "emergencyCall"
+        case .continue:
+            return "continue"
+        }
+    }
+}
+
+func shouldChangeCharacters(in textField: UITextField, range: NSRange = .init(), replacement: String) -> Bool? {
+    textField.delegate?.textField?(textField, shouldChangeCharactersIn: range, replacementString: replacement)
+}
+
+@discardableResult
+func shouldReturn(in textField: UITextField) -> Bool? {
+    textField.delegate?.textFieldShouldReturn?(textField)
+}
+
+func numberOfRows(in tableView: UITableView, section: Int = .zero) -> Int? {
+    tableView.dataSource?.tableView(tableView, numberOfRowsInSection: section)
+}
+
+func cellForRow(in tableView: UITableView, row: Int, section: Int = .zero) -> UITableViewCell? {
+    tableView.dataSource?.tableView(tableView, cellForRowAt: .init(row: row, section: section))
+}
+
+func didSelectRow(in tableView: UITableView, row: Int, section: Int = .zero) {
+    tableView.delegate?.tableView?(tableView, didSelectRowAt: .init(row: row, section: section))
+}
