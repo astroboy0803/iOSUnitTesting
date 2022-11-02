54cff09df89be1af0e9854d64810256c648ffd62
黃宇新 <astroboy0803@gmail.com>
2022-11-02 16:52:20 +0800

    ch16 add check version and simulator

--- 16.TestingViewAppearanceWithSnapshots/TestingViewAppearanceWithSnapshotsTests/SnapshotTests/ViewControllerSnapshotTests.swift
+++ 16.TestingViewAppearanceWithSnapshots/TestingViewAppearanceWithSnapshotsTests/SnapshotTests/ViewControllerSnapshotTests.swift
@@ -2,11 +2,15 @@ import iOSSnapshotTestCase
 @testable import TestingViewAppearanceWithSnapshots
 
 final class ViewControllerSnapshotTests: FBSnapshotTestCase {
-    
     private var sut: ViewController!
     
     override func setUp() {
         super.setUp()
+        
+        let env = ProcessInfo().environment
+        guard let device = env["SIMULATOR_DEVICE_NAME"], let version = env["SIMULATOR_RUNTIME_VERSION"], device.hasSuffix("iPhone 13 Pro") && version == "15.5" else {
+            fatalError("device and version are not match")
+        }
         
         recordMode = false
         
